dcf9279bcd646f30c9ad690b6689fb3276df98df
Deepak-B-G <boppanadeepak1637@gmail.com>
2024-03-15 14:05:35 +0530

    Task-02 is Added

---
+++ Task-02/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const articleRouter = require('./routes/articles')
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+const Article = require('./models/article')
+const methodOverride = require('method-override')
+
+
+const app = express()
+dotenv.config();
+
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster-01.xalmll0.mongodb.net/BlogDB`)
+
+
+
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+app.get('/',async(req, res) => {
+    const articles =  await Article.find().sort({createdAt:'desc'})
+    res.render('articles/index', {articles:articles})
+})
+
+
+
+app.use('/articles', articleRouter)
+app.listen(3000)
